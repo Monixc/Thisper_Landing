6e05e1b0808f11c373b80ccf2b7c69010ec26122
Monixc <chris13404@gmail.com>
2024-02-18 11:14:34 +0900

    [Feat] Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Assets/Logo.svg";
-import { Box } from "@mui/material";
+import { Box, Drawer } from "@mui/material";
+
+import { HiOutlineBars3 } from "react-icons/hi2";
+import InfoIcon from "@mui/icons-material/Info";
+import HomeIcon from "@mui/icons-material/Home";
 const Navbar = () => {
-  return <div></div>;
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOption = [
+    { text: "Home", icon: <HomeIcon /> },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+  ];
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <button className="primary-button">Install Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
