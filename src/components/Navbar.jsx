5ee2458dbf19f7b0d964c4dfa4349b8d1998ddfa
Monixc <chris13404@gmail.com>
2024-02-18 12:51:36 +0900

    [Style] Navbar 반응형

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../Assets/Logo.svg";
+
+import styled, { css } from "styled-components";
 import {
   Box,
   Drawer,
@@ -12,6 +14,92 @@ import {
 import { HiOutlineBars3 } from "react-icons/hi2";
 import InfoIcon from "@mui/icons-material/Info";
 import HomeIcon from "@mui/icons-material/Home";
+
+/*스타일*/
+
+const sizes = {
+  desktop: 1000,
+  tablet: 800,
+};
+
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label]}px) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 5rem;
+  border-bottom: solid 2px #b5b5b5;
+`;
+
+const NavLogoContainer = styled.div`
+  width: 21rem;
+  ${media.desktop`
+  width: 12rem;
+  margin-left: 0.5rem;`}
+
+  ${media.tablet`
+  width: 8rem;
+  `}
+`;
+const NavMenuContainer = styled.div`
+  display: none;
+
+  ${media.tablet`
+  display: flex;
+  `}
+`;
+const NavLinkContainer = styled.div`
+  margin-right: 3rem;
+  text-decoration: none;
+  color: black;
+  font-size: 1.1rem;
+  font-weight: 700;
+
+  ${media.tablet`
+  display: none;
+  `}
+`;
+const NavLink = styled.a`
+  margin-right: 3rem;
+  text-decoration: none;
+  color: black;
+  font-size: 1.1rem;
+  font-weight: 600;
+
+  ${media.desktop`
+  margin-right:1rem;
+  font-size:1rem;`}
+`;
+const PrimaryButton = styled.button`
+  padding: 0.9rem 1.75rem;
+  background-color: #323949;
+  color: white;
+  outline: none;
+  border: none;
+  border-radius: 5rem;
+  font-size: 1rem;
+  cursor: pointer;
+  font-weight: 700;
+  transition: 0.2s;
+
+  ${media.desktop`
+  font-size: 0.8rem;
+  padding: 0.5rem 1.1rem;`}
+
+  &:hover{
+    background-color: rgb(234, 234, 234);}
+  }
+`;
+
+/**/
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOption = [
@@ -22,19 +110,26 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav>
-      <div className="nav-logo-container">
+    <Nav>
+      <NavLogoContainer>
         <img src={Logo} alt="" />
-      </div>
-      <div className="navbar-links-container">
-        <a href="">Home</a>
-        <a href="">About</a>
-        <button className="primary-button">Install Now</button>
-      </div>
-      <div className="navbar-menu-container">
+      </NavLogoContainer>
+      <NavLinkContainer>
+        <NavLink href="">Home</NavLink>
+        <NavLink href="">About</NavLink>
+        <PrimaryButton className="primary-button">Install Now</PrimaryButton>
+      </NavLinkContainer>
+      <NavMenuContainer>
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-      </div>
-    </nav>
+      </NavMenuContainer>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}></Box>
+      </Drawer>
+    </Nav>
   );
 };
 
