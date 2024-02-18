8f58091e960cfd9da4bec68bc11fe9f503075a0f
Monixc <chris13404@gmail.com>
2024-02-18 19:48:25 +0900

    [Feat] Goal 페이지

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import styled, { css } from "styled-components";
+import MainGoal from "../Assets/MainGoal.png";
 
+const GoalContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 50rem;
+  margin-top: -10rem;
+  background-color: #f5f5f5;
+`;
+const ImageContainer = styled.div`
+  max-width: 60rem;
+`;
+const TextContainer = styled.div`
+  margin-top: 5rem;
+  text-align: center;
+  padding-bottom: 5rem;
+`;
+const TitleText = styled.div`
+  font-size: clamp(4rem, 7vw, 5rem);
+  font-weight: 700;
+`;
+const ContentText = styled.div`
+  max-width: 60rem;
+  font-size: clamp(1rem, 3vw, 1.5rem);
+`;
 const Goal = () => {
-  return <div></div>;
+  return (
+    <GoalContainer>
+      <TextContainer>
+        <TitleText>Our Goal</TitleText>
+        <ContentText>
+          We aim to foster diverse opinions and constructive discussions, not
+          just reduce conflicts. We belive in guiding thoughts, rather than
+          enforcing rules.{" "}
+        </ContentText>
+      </TextContainer>
+      <ImageContainer>
+        <img src={MainGoal} alt="" />
+      </ImageContainer>
+    </GoalContainer>
+  );
 };
 
 export default Goal;
